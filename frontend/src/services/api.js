a63f7bcd65facd57e24aee7b07018b5d38ce9ba2
AryamanSi17 <sinhasanjeevkumar08@gmail.com>
2025-11-17 18:04:16 +0530

    updated cors

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -3,7 +3,7 @@ import { toast } from 'react-toastify';
 
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  baseURL: 'https://cv.nextstepsusmle.com/api',
   withCredentials: true,
 });
 
